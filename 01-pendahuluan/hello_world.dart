e129e75d4695cb76c5a99805ec4c0f674d33b34a
ahlaiptek <iptekahla@gmail.com>
2024-03-11 10:44:36 +0700

    Commit

---
+++ 01-pendahuluan/hello_world.dart
@@ -0,0 +1,33 @@
+// main function
+main() {
+	 /* This is the example of multi-line comment 
+    This will print the given statement on screen */  
+	print("Hello World");
+
+	// Dart supports the following built-in Data types:
+	 //    Number
+	 //    Strings
+	 //    Boolean
+	 //    Lists
+	 //    Maps
+	 //    Runes
+	 //    Symbols
+
+	int variabel_angka = 10;
+	double variabel_angka_double = 10.5;
+
+	var variabel_string = "Nama saya Ahla";
+	print(variabel_string);
+
+	bool variabel_boolean = false;
+
+	var variabel_list = [1,2,3,4,5,6];
+
+	var variabel_map = {'nama': 'Ahla'};
+
+	var heart_symbol = '\u2665';  
+    var laugh_symbol = '\u{1f600}';  
+    print(heart_symbol);
+    print(laugh_symbol);
+
+}
